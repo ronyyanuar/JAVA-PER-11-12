74f4a26ca4456a0fcc4a3ef61a1b56b969e0bd68
Harpan20 <regytaharpan20102001@gmail.com>
2019-11-28 15:56:11 +0700

    JAVA-PER-11-12

---
+++ la8_Argumencommanline/src/percobaan1contohprogramcommandline/CommandLineExample.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package percobaan1contohprogramcommandline;
+
+/**
+ *
+ * @author USER
+ */
+public class CommandLineExample {
+     public static void main( String[] args ){       
+         System.out.println("Number of arguments=" + args.length); 
+           System.out.println("First Argument="+ args[0]);
+}
+}
